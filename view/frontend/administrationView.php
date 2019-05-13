e2f8bb0ac0d154653af27eacd93ce87c32ff4c28
Mathieu Callet <callet.mathieu@gmail.com>
2019-05-13 10:19:33 +0200

    Update commentsView and administrationView
    -Update commentsView.php
    Using template, add form to comment post, recover posted comments with new fonction
    -Update administrationView.php
    Create 2 sections, edit posts and moderate comment. prepare buttons for different actions

---
+++ view/frontend/administrationView.php
@@ -0,0 +1,97 @@
+<?php $title = 'Alaskian stories'; ?>
+
+<body>
+
+	<div class="titleblog">
+        <h1>Alaskian stories</h1>
+    </div>
+
+    <?php ob_start(); ?>
+	
+	<div class='card'>
+                <h2>Edit posts</h2>
+                
+                <?php
+
+                $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+
+                $req = $db->query('SELECT id, title, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM posts ORDER BY creation_date');
+                ?>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Content</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while ($data = $req->fetch())
+                        {
+                            ?>
+                            <tr>
+                                <td><strong><?php echo $data['title']; ?></strong> - </td>
+                                <td><em><?php echo $data['creation_date_fr']; ?></em> - </td>
+                                <td>
+                                    <a class="btn" href="index.php?action=updatePostView&id=<?= $data['id'] ?>">Modify post</a>
+                                </td>
+                                <td>
+                                    <a class="btn" href="index.php?action=deletePost&id=<?= $data['id'] ?>">Delete post</a>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+
+                        $req->closeCursor();
+
+                        ?>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td>
+                                <a class="btn" href="index.php?action=createPostView">Create post</a>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+
+                <div class='card'>
+                    <h2>Moderate comments</h2>
+                    <?php
+                    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+                    $req2 = $db->query('SELECT id, author, comment, comment_date, nb_report FROM comments WHERE nb_report > 0');
+                    ?>
+                    <table>
+                        <tr>
+                            <th>Author</th>
+                            <th>Comment</th>
+                            <th>Date</th>
+                            <th>Report count</th>
+                        </tr>
+                        <?php
+                        while ($data = $req2->fetch())
+                        {
+                            ?>
+                            <tr>
+                                <td><strong><?php echo $data['author']; ?></strong> - </td>
+                                <td><?php echo $data['comment']; ?> - </td>
+                                <td><?php echo $data['comment_date']; ?> - </td>
+                                <td><?php echo $data['nb_report']; ?>  </td>
+                                <td>
+                                    <a class="btn" href="index.php?action=validateComment&id=<?= $data['id'] ?>">Validate comment</a>
+                                </td>
+                                <td>
+                                    <a class="btn" href="index.php?action=deleteComment&id=<?= $data['id'] ?>">Delete comment</a>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+                        $req2->closeCursor();
+                        ?>
+                    </table>
+                </div>
+
+                <?php $content = ob_get_clean(); ?>
+				<?php require('view/frontend/template.php'); ?>
+
+            </body>
---
+++ view/frontend/commentsView.php
@@ -0,0 +1,54 @@
+<?php $title = 'Alaskian stories'; ?>
+
+    <body>
+    
+    <?php ob_start(); ?>
+
+    	 <article class="card">
+            <h3><?= htmlspecialchars($post['title']) ?></h3>
+            <h6><em>le <?= $post['creation_date_fr'] ?></em></h6>
+			<div class="image"><img src="public/images/draw.png"/></div><br />
+			<div class="chapter">
+                <?= nl2br(htmlspecialchars($post['content'])) ?>
+            </div>    
+        </article>
+
+		<h2>New comment</h2><br/>
+
+            <form action="index.php?action=addComment&id=<?= $post['id'] ?>" method="post">
+                <div>
+                    <label for="author">Author</label><br />
+                    <input type="text" id="author" name="author" />
+                </div>
+                <div>
+                    <label for="comment">Comment</label><br />
+                    <textarea id="comment" name="comment"></textarea>
+                </div>
+                <div>
+                    <input type="submit" />
+                </div>
+            </form>
+
+        <h2>Posted comments</h2>
+
+            <?php
+            while ($comment = $comments->fetch())
+            {
+                ?>
+                <p class="comAuthor"><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+                <?= $comment['nb_report'] > 0 ? '<em>Ce commentaire a été reporté.</em>' : nl2br(htmlspecialchars($comment['comment'])) ?>
+
+                    <form method="POST" action="index.php?action=reportComment">
+                    <input type="hidden" name="comment_id" value="<?= $comment['id'] ?>"/>
+                    <input type="submit" name="report" value="Report comment"/>
+                    </form>
+
+                    <?php
+                }
+                ?>
+
+    <?php $content = ob_get_clean(); ?>
+	<?php require('view/frontend/template.php'); ?>
+
+    </body>
+
