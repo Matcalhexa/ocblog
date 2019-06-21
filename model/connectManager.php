3337513a47a626188b9c530d6542dfa858eda8dd
Mathieu Callet <callet.mathieu@gmail.com>
2019-06-21 15:21:23 +0200

    secure connect function dbConnect

--- model/connectManager.php
+++ model/connectManager.php
@@ -3,7 +3,11 @@ class Manager
 {
 	protected function dbConnect()
 	{
-		$db = new PDO('mysql:host=db5000094081.hosting-data.io;dbname=dbs88680;charset=utf8', 'dbu296893', 'BlOgJf30210*');
+		$config = parse_ini_file('env.ini' , true);
+		$user = $config['db_username'];
+		$pass = $config['db_password'];
+
+		$db = new PDO('mysql:host=db5000094081.hosting-data.io;dbname=dbs88680;charset=utf8', $user, $pass);
 		return $db;
 	}
 }
